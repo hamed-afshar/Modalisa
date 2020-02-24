a602b22393b31415a1699ce76725a46abc9af1ca
hamed-afshar <hamed.afshar.n@gmail.com>
2020-02-24 16:37:43 +0330

    Design waiting-for-confirmation page

--- resources/views/others/pending-for-confirmation.blade.php
+++ resources/views/others/pending-for-confirmation.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Please wait for confirmation</h1>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Waiting For Confirmation</h1>
+            <p class="lead"> Now we are checking your information </p>
+            <hr class="my-4">
+            <p> We are very glad to have you as our reliable partner, so that our team will review your account and contact you in a short time! </p>
+        </div>
+
+    </div>
+
 @endsection
 
 
