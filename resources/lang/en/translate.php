40e25e38b21b0e6196d607b1e4803622a0e3c555
hamed-afshar <hamed.afshar.n@gmail.com>
2020-02-27 01:36:35 +0330

    english translate file cretaed

---
+++ resources/lang/en/translate.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    "home" => "Home",
+    "about" => "About Us",
+    "contact" => "Contact Us",
+    "faq" => "FAQ",
+    "footer_text" => "We as a company in Istanbul,Turkey are looking to have a reliable relation with
+                      retailers who want to buy online products from online shops in Turkey. Our team
+                      is always available to give you best possible service and help you to growth your
+                      business. Now register to our website and take benefit from our integreated ordering system!",
+    "register" => "Register",
+    "all_right_reserved" => "All Rights Reserved",
+    "powered_by_modalisa" => "Powered By Modalisa",
+    "pending_for_confirmation_title" => "Waiting For Confirmation",
+    "pending_for_confirmation_text1" => "Now we are checking your information",
+    "pending_for_confirmation_text2" => "We are very glad to have you as our reliable partner,
+                                         so that our team will review your account and contact
+                                         you in a short time!",
+];
