fe58f69494076be52f25869b293caefc5e25de80
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-01 18:35:36 +0330

    Create Roll model
    Create Model, Controller and migration

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\AccessProvider;
 use App\Role;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class RoleController extends Controller
 {
     //store role instance in db
     public function store()
     {
-        $data = request([
-           'name' => 'name'
-        ]);
-        Role::create($data);
+       // $accessProvider = new AccessProvider(auth()->user()->id, 'create-role');
+        dd(DB::table('roles')->where('id', 1)->first()->name);
+
+        Role::create(request([
+            'name' => 'name'
+        ]));
     }
+
 }
