125f01efc7c14bca41f70ae82ba9ff2d1b74b4d4
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-02 18:49:46 +0330

    Create roles index file

---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+<body>
+<h1> Modalisa </h1>
+<ul>
+    @forelse($roles as $role)
+        <li>
+            {{ $role->name }}
+        </li>
+    @empty
+        <li> No roles Yet </li>
+    @endforelse
+</ul>
+
+</body>
+</html>
