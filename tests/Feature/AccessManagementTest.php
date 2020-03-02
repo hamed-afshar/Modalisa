4ea124ac0837b7ae29bfa38d119e721263e08393
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-02 18:51:37 +0330

    Update AccessManagementTest.php

--- tests/Feature/AccessManagementTest.php
+++ tests/Feature/AccessManagementTest.php
@@ -3,8 +3,10 @@
 namespace Tests\Feature;
 
 use App\Role;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class AccessManagementTest extends TestCase
@@ -15,10 +17,23 @@ class AccessManagementTest extends TestCase
     public function only_SystemAdmin_can_create_role()
     {
         $this->withoutExceptionHandling();
-        $this->actingAs(factory('App\User')->create());
+        $userRole = factory('App\UserRole')->create();
+        $user = User::find($userRole->user_id);
+        $this->actingAs($user);
+        $rolePermission = factory('App\RolePermission')->create();
         $attributes = factory('App\Role')->raw();
-        $this->post('/role', $attributes);
+        $this->post('/roles', $attributes);
         $this->assertDatabaseHas('roles', $attributes);
+    }
+
+
+    /** @test */
+    public function only_SystemAdmin_can_view_roles()
+    {
+        $this->withoutExceptionHandling();
+        $this->actingAs(factory('App\User')->create());
+        $role = factory('App\Role')->create();
+        $this->get('/roles')->assertSee($role->name);
     }
 
     /** @test */
