03aaf7222dc54bf2d51ae2d37d4be9df1707d267
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-07 10:19:46 +0330

    Update create.blade.php

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -2,12 +2,18 @@
 <html>
 <head>
     <title>
-
+        C
     </title>
 </head>
 <body>
-<h1> Modalisa </h1>
-
+<h1> Roles </h1>
+<form action="" method="">
+    <div class="input-group">
+        <input type="text", name="name">
+    </div>
+    <button type = "submit">Add Role</button>
+    @csrf
+</form>
 
 </body>
 </html>
