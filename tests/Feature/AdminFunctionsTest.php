6d546c7436fcc7960fc86bd029581360334d29e8
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-09 01:41:09 +0330

    Add AdminFunctionTest
    This test is related to admin functionalities

---
+++ tests/Feature/AdminFunctionsTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AdminFunctionsTest extends TestCase
+{
+    /** @test */
+    public function SystemAdmin_can_confirm_or_lock_users()
+    {
+
+    }
+
+    /** @test */
+    public function SystemAdmin_can_delete_users()
+    {
+
+    }
+
+}
