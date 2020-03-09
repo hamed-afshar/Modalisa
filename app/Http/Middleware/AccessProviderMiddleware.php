73df52acf098acd96b8f21f7a39263cf6c7ae80f
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-09 17:24:13 +0330

    Update AccessProviderMiddleware.php

--- app/Http/Middleware/AccessProviderMiddleware.php
+++ app/Http/Middleware/AccessProviderMiddleware.php
@@ -15,11 +15,62 @@ class AccessProviderMiddleware
      * @param $role
      * @return mixed
      */
-    public function handle($request, Closure $next, $requestedAccess)
+    public function handle($request, Closure $next, $requestedAccess, $requestedPage)
     {
         $accessProvider = new AccessProvider($request->user()->id, $requestedAccess);
+
+        //user is not confirmed yet
+        if ($accessProvider->getPermission() == 'not-confirmed') {
+            route('pending-for-confirmation');
+        }
+
+        //user is locked
+        if ($accessProvider->getPermission() == 'locked') {
+            route('pending-for-confirmation');
+        }
+
+        // check permissions
         if ($accessProvider->getPermission()) {
-            route('all-users');
+            switch ($requestedPage) {
+                case 'all-users' :
+                    route('all-users');
+                    break;
+                case 'show-user':
+                    route('show-user');
+                    break;
+                case 'user-edit-form':
+                    route('user-edit-form');
+                    break;
+                case 'user-profile':
+                    route('user-profile');
+                    break;
+                case 'user-delete':
+                    route('user-delete');
+                    break;
+                case 'all-roles':
+                    route('all-roles');
+                    break;
+                case 'role-create-form':
+                    route('role-create-form');
+                    break;
+                case 'save-role':
+                    route('save-role');
+                    break;
+                case 'show-role':
+                    route('show-role');
+                    break;
+                case'role-edit-form':
+                    route('role-edit-form');
+                    break;
+                case 'update-role':
+                    route('update-role');
+                    break;
+                case 'delete-role':
+                    route('delete-role');
+                    break;
+                default:
+            }
+
         } else {
             return $accessProvider->accessDenied();
         }
