1d153f05bf8a0c4bb7b887960b4529ba529a3402
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-09 17:24:32 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,31 @@
  */
 
 
-Route::post('/users', 'UserController@store');
-Route::get('/users/create', 'UserController@create');
-Route::delete('/users', 'UserController@destroy');
-Route::get('/pending-for-confirmation', 'UserController@showPendingForConfirmation');
+Route::post('/users', 'UserController@store')->name('user-register');
+Route::get('/users/create', 'UserController@create')->name('user-register-form');
+Route::get('/pending-for-confirmation', 'UserController@showPendingForConfirmation')->name('pending-for-confirmation');
+Route::get('/locked', 'UserController@showLocked')->name('show-locked');
 
 Route::group(['middleware' => 'auth'], function() {
+    Route::get('/users', 'UserController@index')->name('all-users')->middleware('AccessProvider:see-users, all-users');
+    Route::get('/users/{user}', 'UserController@show')->name('show-user')->middleware('AccessProvider:see-users, show-user');
+    Route::get('/users/{user}/edit', 'UserController@edit')->name('user-edit-form')->middleware('AccessProvider:edit-profile, user-edit-form');
+    Route::patch('/users/{user}', 'UserController@update')->name('user-profile')->middleware('AccessProvider:edit-profile, user-profile');
+    Route::delete('/users/{user}', 'UserController@destroy')->name('user-delete')->middleware('AccessProvider:delete-user, user-delete');
+
+    Route::get('/roles', 'RoleController@index')->name('all-roles')->middleware('AccessProvider:see-roles, all-roles');
+    Route::get('/roles/create', 'RoleController@create')->name('role-create-form')->middleware('AccessProvider:create-roles, role-create-form');
+    Route::post('/roles', 'RoleController@store')->name('save-role')->middleware('AccessProvider:create-roles, save-role');
+    Route::get('/roles/{role}', 'RoleController@show')->name('show-role')->middleware('AccessProvider:see-roles, show-role');
+    Route::get('/roles/{role}/edit', 'RoleController@edit')->name('role-edit-form')->middleware('AccessProvider:edit-roles, role-edit-form');
+    Route::patch('/roles/{role}', 'RoleController@update')->name('update-role')->middleware('AccessProvider:edit-roles, update-role');
+    Route::delete('/roles/{role}', 'RoleController@destroy')->name('delete-role')->middleware('AccessProvider:delete-roles, delete-role');
+
     Route::get('/orders', 'OrdersController@index');
     Route::get('/orders/{order}', 'OrdersController@show');
     Route::post('/orders', 'OrdersController@store');
 
-    Route::get('/users', 'UserController@index')->name('all-users')->middleware('AccessProvider:see-users');
-    Route::get('/users/{user}', 'UserController@show')->name('show-user')->middleware('AccessProvider:see-users');
-    Route::get('/users/{user}/edit', 'UserController@edit')->name('user-edit-form')->middleware('AccessProvider:edit-profile');
-    Route::patch('/users/{user}', 'UserController@update')->name('user-profile')->middleware('AccessProvider:edit-profile');
-    Route::delete('/users/{user}', 'UserController@destroy')->name('user-delete')->middleware('AccessProvider:delete-user');
+
 
 
     Route::get('/subscriptions', 'SubscriptionController@index');
@@ -35,13 +45,6 @@ Route::group(['middleware' => 'auth'], function() {
     Route::post('/user-subscription', 'SubscriptionController@assignSubscription');
     Route::get('/user-subscription', 'SubscriptionController@indexUserSubscription');
 
-    Route::get('/roles', 'RoleController@index');
-    Route::post('/roles', 'RoleController@store');
-    Route::get('/roles/create', 'RoleController@create');
-    Route::get('/roles/{role}', 'RoleController@show');
-    Route::get('/roles/{role}/edit', 'RoleController@edit');
-    Route::patch('/roles/{role}', 'RoleController@update');
-    Route::delete('/roles/{role}', 'RoleController@destroy');
 
 
     Route::get('/home', 'HomeController@index')->name('home');
