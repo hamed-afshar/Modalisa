01513b16af5cf74e85c85266be7806c401a88adb
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-14 21:21:34 +0330

    Update Composer on Laptop

--- public/js/app.js
+++ public/js/app.js
@@ -68998,8 +68998,8 @@ if (document.getElementById('example')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Hamed\PhpstormProjects\Modalisa\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Hamed\PhpstormProjects\Modalisa\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\shadi\Documents\GitHub\Modalisa\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\shadi\Documents\GitHub\Modalisa\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
