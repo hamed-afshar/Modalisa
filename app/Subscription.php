462ec6f09eb650c9b58a7ad7f367f7cd0e4daf1e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-16 16:59:41 +0330

    Update Subscription.php

--- app/Subscription.php
+++ app/Subscription.php
@@ -9,6 +9,12 @@ class Subscription extends Model
 {
     protected $guarded = [];
 
+    // each subscription has many assigned users
+    public function userSubscriptions()
+    {
+        return $this->hasMany('App\UserSubscription');
+    }
+
     //return permissions path
     public function path()
     {
