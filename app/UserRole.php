9ba2729a23801cfbb96db19291fde5c599476b9e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-17 15:00:47 +0330

    Update UserRole.php

--- app/UserRole.php
+++ app/UserRole.php
@@ -8,6 +8,12 @@ class UserRole extends Model
 {
     protected $guarded = [];
 
+    //each role assigned to one user
+    public function roleOwner()
+    {
+        return $this->belongsTo('App\Role')->withDefault();
+    }
+
     //each role belongs to one user
     public function user()
     {
