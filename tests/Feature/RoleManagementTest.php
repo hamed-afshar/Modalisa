2f8b82cb5481759e5cee2063d64a980780bbed97
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-19 01:19:47 +0330

    Update RoleManagementTest.php

--- tests/Feature/RoleManagementTest.php
+++ tests/Feature/RoleManagementTest.php
@@ -20,7 +20,7 @@ class RoleManagementTest extends TestCase
         $user = factory('App\User')->create(['id' => '1', 'confirmed' => $confirmed, 'locked' => $locked]);
         $role = Role::create(['id' => 1, 'name' => $role]);
         $permission = Permission::create(['id' => 1, 'name' => $request]);
-        $userRole = $user->roles()->create(['user_id' => $user->id, 'role_id' => $role->id]);
+        $userRole = $user->role()->create(['user_id' => $user->id, 'role_id' => $role->id]);
         $rolePermission = $role->assignedPermissions()->create(['role_id' => $role->id, 'permission_id' => $permission->id]);
         $this->actingAs($user);
     }
@@ -30,7 +30,7 @@ class RoleManagementTest extends TestCase
         $user = factory('App\User')->create(['id' => '1', 'confirmed' => $confirmed, 'locked' => $locked]);
         $role = Role::create(['id' => 1, 'name' => $role]);
         $permission = Permission::create(['id' => 1, 'name' => $request]);
-        $userRole = $user->roles()->create(['user_id' => $user->id, 'role_id' => $role->id]);
+        $userRole = $user->role()->create(['user_id' => $user->id, 'role_id' => $role->id]);
         $rolePermission = $role->assignedPermissions()->create(['role_id' => $role->id, 'permission_id' => $permission->id]);
         $this->actingAs($user);
     }
@@ -109,6 +109,96 @@ class RoleManagementTest extends TestCase
     {
         $userRole = factory('App\UserRole')->create();
         $this->assertInstanceOf('App\Role', $userRole->roleOwner);
+    }
+
+    /** @test */
+    public function UserRole_belongs_to_one_user()
+    {
+        $userRole = factory('App\UserRole')->create();
+        $this->assertInstanceOf('App\User', $userRole->userOwner);
+    }
+
+    /** @test */
+    public function SystemAdmin_can_see_all_roles_assigned_to_users()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $userRole = factory('App\UserRole')->create();
+        $this->get('/user-roles')->assertSeeTextInOrder(array($userRole->id, $userRole->user_id, $userRole->role_id));
+    }
+
+    /** @test */
+    public function form_is_available_to_assign_a_role_to_user()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $this->get('/user-roles/create')->assertOk();
+
+    }
+
+    /** @test */
+    public function SystemAdmin_can_assigned_a_role_to_user()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $attributes = factory('App\UserRole')->raw();
+        $this->post('/user-roles', $attributes)->assertRedirect('/user-roles');
+        $this->assertDatabaseHas('user_roles', $attributes);
+    }
+
+    /** @test */
+    public function user_id_is_required()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $attributes = factory('App\UserRole')->raw(['user_id' => '']);
+        $this->post('/user-roles', $attributes)->assertSessionHasErrors('user_id');
+    }
+
+    /** @test */
+    public function role_id_is_required()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $attributes = factory('App\UserRole')->raw(['role_id' => '']);
+        $this->post('/user-roles', $attributes)->assertSessionHasErrors('role_id');
+    }
+
+    /** @test */
+    public function SystemAdmin_can_view_a_single_user_role()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $userRole = factory('App\UserRole')->create();
+        $this->get($userRole->path())->assertSeeTextInOrder(array($userRole->id, $userRole->user_id, $userRole->role_id));
+    }
+
+    /** @test */
+    public function form_is_available_to_edit_a_role_assigned_to_user()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $userRole = factory('App\UserRole')->create();
+        $this->get($userRole->path().'/edit')->assertSeeTextInOrder(array($userRole->id, $userRole->user_id, $userRole->role_id));
+    }
+
+    /** @test */
+    public function SystemAdmin_can_update_a_role_assigned_to_user()
+    {
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $newUser = factory('App\User')->create(['id' => 15]);
+        $newRole = factory('App\Role')->create(['id' => 25]);
+        $userRole = factory('App\UserRole')->create();
+        $newAttributes = [
+            'user_id' => $newUser->id,
+            'role_id' => $newRole->id
+        ];
+        $this->patch($userRole->path(), $newAttributes)->assertRedirect($userRole->path());
+        $this->assertEquals($newAttributes['user_id'], UserRole::where('id', $userRole->id)->value('user_id'));
+        $this->assertEquals($newAttributes['role_id'], UserRole::where('id', $userRole->id)->value('role_id'));
+    }
+
+    /** @test */
+    public function SystemAdmin_can_delete_a_role_assigned_to_user()
+    {
+        $this->withoutExceptionHandling();
+        $this->prepare_SystemAdmin_env('SystemAdmin', 'edit-roles', 1, 0);
+        $userRole = factory('App\UserRole')->create();
+        $this->delete($userRole->path())->assertRedirect('/user-roles');
+        $this->assertDatabaseMissing('user_roles', ['id' => $userRole->id]);
     }
 
 
