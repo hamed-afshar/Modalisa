03a8385dde75462678e471b23885436c941f0ca1
hamed-afshar <hamed.afshar.n@gmail.com>
2020-03-24 13:26:16 +0430

    Update Permission.php

--- app/Permission.php
+++ app/Permission.php
@@ -7,8 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Permission extends Model
 {
     protected $guarded = [];
+
+    //return a path
     public function path()
     {
         return "/permissions/{$this->id}";
+    }
+
+    public function rolePermissions()
+    {
+        return $this->hasMany('App\RolePermission');
     }
 }
