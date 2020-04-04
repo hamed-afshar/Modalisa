9cdf77808f78c51ef8fbbf721b2e2d5722fb37fb
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-04 17:54:50 +0430

    Create _navbar.blade.php

---
+++ resources/views/_navbar.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container mx-auto flex items-center">
+        <nav class="w-2/5">
+            <a href="#"> <i class="fa fa-home fa-lg"></i> Home </a>
+        </nav>
+        <div class="w-1/5"></div>
+        <div class="w-2/5"></div>
+    </div>
+@endsection
+
