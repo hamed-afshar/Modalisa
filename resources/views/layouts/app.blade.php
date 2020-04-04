da866a8d737c8180ac81f810c9b48eae9243f61a
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-05 01:42:56 +0430

    Change sass PreProcessor to just css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="/fontawesome/all.min.css" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body class="bg-gray-100">
 @yield('content')
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,11 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-const tailwindcss = require('tailwindcss')
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false,
-        postCss: [tailwindcss('tailwind.config.js')],
-    });
+    .postCss('resources/css/main.css', 'public/css', [
+        require('tailwindcss'),
+    ])
