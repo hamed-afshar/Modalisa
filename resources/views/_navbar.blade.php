a1e8c3485d829f60c1423c9785634e8acc8ea2bf
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-05 00:52:27 +0430

    Update _navbar.blade.php

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container mx-auto flex items-center">
-        <nav class="w-2/5">
-            <a href="#"> <i class="fa fa-home fa-lg"></i> Home </a>
-        </nav>
-        <div class="w-1/5"></div>
-        <div class="w-2/5"></div>
+    <div class="bg-white">
+        <div class="container mx-auto flex bg-white p-2">
+            <nav class="w-2/5">
+                <a href="#"> <i class="fa fa-home fa-sm"></i> Home </a>
+            </nav>
+            <div class="w-1/5">
+                <h1 class="text-center"> Modalisa </h1>
+            </div>
+            <div class="w-2/5 test"></div>
+        </div>
     </div>
 @endsection
 
