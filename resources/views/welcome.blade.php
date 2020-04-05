91a22403eba257ee4f65d29456ba17befddaa671
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-05 19:43:54 +0430

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container flex items-center mx-auto">
-
+    <div class="h-screen">
+        <div class="bg-gray-400 flex h-full items-center justify-center">
+            <h1 class="text-xl"> Modalisa </h1>
+            <div class="flex">
+                <div class="w-1/2 flex justify-end mr-8">
+                    <a href="#" class="text-sm"> Login </a>
+                </div>
+                <div class="w-1/2 flex justify-start">
+                    <a href="#" class="text-sm"> Sign Up </a>
+                </div>
+            </div>
+        </div>
     </div>
+
 @endsection
 
