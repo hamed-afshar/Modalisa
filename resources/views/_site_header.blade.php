80e04250e1ce6f67aa08c8b8de691b7919a80665
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-05 19:43:59 +0430

    Create _site_header.blade.php

---
+++ resources/views/_site_header.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+    <div class="bg-white shadow-lg">
+        <div class="container bg-white mx-auto flex p-4">
+            <div class="w-1/2 md:flex">
+                <h1 class="text-lg"> Modalisa </h1>
+            </div>
+            <div class="w-1/2 md:flex justify-end">
+                <button class="info-button"> Login </button>
+                <button class="info-button ml-2"> Sign Up </button>
+            </div>
+        </div> <!-- end container -->
+    </div>
+@endsection
