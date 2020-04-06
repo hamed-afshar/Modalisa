fa1a9fd451b7902a094a0b7bbacedcb98cbcfce0
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-07 00:38:14 +0430

    Update _site_header.blade.php

--- resources/views/_site_header.blade.php
+++ resources/views/_site_header.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
     <div class="bg-white shadow-lg">
-        <div class="container bg-white mx-auto flex p-4">
-            <div class="w-1/2 md:flex">
-                <h1 class="text-lg"> Modalisa </h1>
+        <div class="container bg-white flex flex-wrap mx-auto p-4">
+            <div class="w-1/2">
+                <h1 class="logo-medium"> Modalisa </h1>
             </div>
-            <div class="w-1/2 md:flex justify-end">
-                <button class="info-button"> Login </button>
-                <button class="info-button ml-2"> Sign Up </button>
+            <div class="flex-wrap md:flex justify-end">
+                <button class="info-button w-full md:w-1/2"> Login </button>
+                <button class="info-button w-full md:w-1/2 ml-2"> Sign Up </button>
             </div>
         </div> <!-- end container -->
     </div>
