e3c03e43012403a76002e33cecdb62f792ab1ed8
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-07 00:38:12 +0430

    Update _navbar.blade.php

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 @section('content')
     <div class="bg-white shadow-lg">
-        <div class="container mx-auto flex bg-white p-2">
-            <nav class="w-2/5">
-                <a href="#" class="top-navbar-item"> <i class="fa fa-home fa-lg"></i> Home </a>
-                <a href="#" class="top-navbar-item ml-8"> <i class="fab fa-whatsapp fa-lg>"></i> Contact Us </a>
-            </nav>
-            <div class="w-1/5">
-                <h1 class="text-center font-extrabold text-xl"> Modalisa </h1>
-            </div>
-            <div class="w-2/5 flex justify-end">
-                <a href="#" class="ml-4 top-navbar-item"><i class="fa fa-bell fa-lg"></i> </a>
-                <a href="#" class="ml-4 top-navbar-item"><i class="fa fa-envelope fa-lg"></i> </a>
-                <a href="#" class="ml-12 top-navbar-item"><i class="fa fa-user fa-lg"></i> </a>
+        <div class="bg-white container mx-auto p-2">
+            <div class="flex flex-wrap">
+                <div class="w-full">
+                    <h1 class="logo-medium text-center"> Modalisa </h1>
+                </div>
+                <nav class="w-full flex justify-center">
+                    <a href="#" class="ml-4 top-navbar-item"><i class="far fa-bell fa-lg"></i> </a>
+                    <a href="#" class="ml-4 top-navbar-item"><i class="far fa-envelope fa-lg"></i> </a>
+                    <a href="#" class="ml-12 top-navbar-item"><i class="far fa-user fa-lg"></i> </a>
+                </nav>
             </div>
         </div> <!-- end container -->
     </div>
