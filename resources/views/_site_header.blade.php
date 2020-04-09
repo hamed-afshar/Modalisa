a1be080920e74840c3dd2316a536dcda979bb99f
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-09 12:06:47 +0430

    Update _site_header.blade.php

--- resources/views/_site_header.blade.php
+++ resources/views/_site_header.blade.php
@@ -6,8 +6,8 @@
                 <h1 class="logo-medium"> Modalisa </h1>
             </div>
             <div class="flex flex-wrap justify-end">
-                <button class="info-button md:w-1/4 lg:w-1/6 w-full block"> Login </button>
-                <button class="info-button mt-2 md:mt-0 md:ml-2 w-full md:w-1/4 lg:w-1/6"> Sign Up </button>
+                <button class="btn-green md:w-1/4 lg:w-1/6 w-full block"> Login </button>
+                <button class="btn-green mt-2 md:mt-0 md:ml-2 w-full md:w-1/4 lg:w-1/6"> Sign Up </button>
             </div>
         </div> <!-- end container -->
     </div>
