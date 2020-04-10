f72dcdcc36dd379f5a4ba86248448c9afd56fc9e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-11 01:58:17 +0430

    change create2 to create

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,121 +1,115 @@
 @include('_site_header')
-
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Register') }}</div>
-
-                    <div class="card-body">
-                        <form method="POST" action="/users">
-                            @csrf
-
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="name" type="text"
-                                           class="form-control @error('name') is-invalid @enderror" name="name"
-                                           value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="email" type="email"
-                                           class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email">
-
-                                    @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="country"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Country') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="country" type="country" class="form-control" name="country">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="language"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Language') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="language" type="language" class="form-control" name="language">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="tel" class="col-md-4 col-form-label text-md-right">{{ __('Tel') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="tel" type="tel" class="form-control" name="tel">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="communication_media" class="col-md-4 col-form-label text-md-right">{{ __('Social Media') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="communication_media" type="socialID" class="form-control" name="communication_media">
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password"
-                                           class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="new-password">
-
-                                    @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                    @enderror
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                <label for="password-confirm"
-                                       class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                                <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                           name="password_confirmation" required autocomplete="new-password">
-                                </div>
-                            </div>
-
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+<div class="container max-w-xs md:max-w-full mx-auto mt-16">
+    <div class="grid grid-cols-1 md:grid-cols-6 grid-rows-6">
+        <div class="col-start-3 col-span-2 row-start-2 row-end-5">
+            <div class="flex flex-wrap justify-center">
+                <div
+                    class="inline-block bg-pink-800 border border-solid rounded-full px-8 py-4 transform translate-y-1/2 shadow-xl text-white font-bold">
+                    Register
                 </div>
             </div>
+            <form method="POST" action="{{route('users.create')}}" class="bg-white shadow-md rounded px-8 py-8">
+                @csrf
+                <div class="flex flex-wrap mb-6 mt-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="name"> {{  __('Full Name')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <input class="input-text w-full"
+                               type="text" id="name" name="name" placeholder="Alex Morgan">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="email"> {{  __('Email')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <input class="input-text w-full" type="email" id="email" name="email"
+                               placeholder="YourEmail@example.com">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="mobile"> {{  __('Mobile')  }}</label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <input class="input-text w-full" type="tel" id="mobile" name="mobile"
+                               placeholder="00989123463474">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="country"> {{  __('Country')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <select
+                            class="input-option w-full"
+                            id="country" name="country">
+                            <option selected disabled>Country</option>
+                            <option value="Iran"> Iran</option>
+                            <option value="Turkey"> Turkey</option>
+                            <option value="Ukraine"> Ukraine</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="language"> {{  __('Language')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <select
+                            class="input-option w-full"
+                            id="language" name="language">
+                            <option disabled selected>Language</option>
+                            <option value="Iran"> Persian</option>
+                            <option value="Turkey"> Turkish</option>
+                            <option value="Ukraine"> Russian</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="media"> {{  __('Social Media')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <select
+                            class="input-option w-full"
+                            id="media" name="media">
+                            <option disabled selected>Communication Media</option>
+                            <option value="Iran"> Whatsapp</option>
+                            <option value="Turkey"> Telegram</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="password"> {{  __('Password')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <input class="input-text w-full" type="password" id="password" name="password"
+                               placeholder="********">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mb-6">
+                    <div class="w-full lg:w-1/3">
+                        <label class="label mb-1 py-1" for="passwordConfirm"> {{  __('Password Confirm')  }} </label>
+                    </div>
+                    <div class="w-full lg:w-2/3">
+                        <input class="input-text w-full" type="password" id="passwordConfirm" name="passwordConfirm"
+                               placeholder="********">
+                    </div>
+                </div>
+                <div class="flex">
+                    <button class="btn-pink uppercase shadow-xl w-full">Signup</button>
+                </div>
+                <div class="flex justify-center mt-2 w-full">
+                    <div class="label pt-2">Alredy have an account?</div>
+                </div>
+                <div class="flex justify-center w-full">
+                    <a href="#" class="text-blue-400 font-bold hover:text-blue-600 p-1">Login</a>
+                </div>
+            </form>
         </div>
     </div>
-@endsection
+</div>
