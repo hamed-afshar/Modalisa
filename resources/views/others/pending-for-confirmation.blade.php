f6b7918c4a16e7a6679ed6abce4a7b052f55feb2
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-11 19:38:52 +0430

    Update pending-for-confirmation.blade.php

--- resources/views/others/pending-for-confirmation.blade.php
+++ resources/views/others/pending-for-confirmation.blade.php
@@ -1,16 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="jumbotron">
-            <h1 class="display-4">{{ __("translate.pending_for_confirmation_title") }}</h1>
-            <p class="lead"> {{ __("translate.pending_for_confirmation_text1") }}</p>
-            <hr class="my-4">
-            <p> {{ __("translate.pending_for_confirmation_text2") }} </p>
-        </div>
-
-    </div>
-    @include('layouts.footer')
-
+  <h1> Pending for confirmation </h1>
 @endsection
 
 
