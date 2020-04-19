9202fdd9bf93641591339a8256aca90806fdb728
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-19 17:11:08 +0430

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use App\AccessProvider;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -14,46 +14,15 @@ class UserController extends Controller
     //index users
     public function index()
     {
-        $authUser = auth()->user();
-        $this->authorize('index', $authUser);
+        $this->authorize('index', auth()->user());
         $users = User::all();
         return view('users.index', compact('users'));
-    }
-
-    //form to create a user
-    public function create()
-    {
-        return view('users.create');
-    }
-
-    // store users
-    public function store()
-    {
-        $data = request()->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required',
-            'language' => 'required',
-            'tel' => 'required',
-            'country' => 'required',
-            'communication_media' => 'required'
-        ]);
-        User::create([
-            'name' => request('name'),
-            'email' => request('email'),
-            'password' => Hash::make($data['password']),
-            'last_ip' => request()->ip(),
-            'language' => request('language'),
-            'tel' => request('tel'),
-            'country' => request('country'),
-            'communication_media' => request('communication_media')
-        ]);
-        return redirect()->route('pending');
     }
 
     //systemadmin can see a single user
     public function show(User $user)
     {
+        $this->authorize('show', auth()->user());
         return view('users.show', compact('user'));
     }
 
@@ -66,6 +35,7 @@ class UserController extends Controller
     //update a user information
     public function update(User $user)
     {
+        $this->authorize('update', $user, auth()->user());
         $data = request()->validate([
             'email' => 'required',
             'password' => 'required',
@@ -80,12 +50,7 @@ class UserController extends Controller
 // delete a user
     public function destroy(User $user)
     {
+        $this->authorize('destroy', auth()->user());
         return redirect('access-denied');
-    }
-
-//show pending for confirmation page
-    public function pending()
-    {
-        return view('/others/pending-for-confirmation');
     }
 }
