4059a8bd5781562ad77eb338e3afc9635d1dd74e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-19 17:12:07 +0430

    Update AdminFunctionsTest.php

--- tests/Unit/AdminFunctionsTest.php
+++ tests/Unit/AdminFunctionsTest.php
@@ -12,10 +12,26 @@ use Tests\TestCase;
 
 class AdminFunctionsTest extends TestCase
 {
+    use WithFaker,
+        RefreshDatabase;
+
     /** @test */
     public function SystemAdmin_can_assign_a_role_to_user()
     {
+        $user = factory('App\User')->create();
+        $role = factory('App\Role')->create();
+        $user->assignRole($role);
+        $this->assertDatabaseHas('user_roles', ['user_id' => $user->id, 'role_id' => $role->id]);
+    }
 
+    /** @test */
+    public function SystemAdmin_can_assign_permission_to_role()
+    {
+        $this->withoutExceptionHandling();
+        $role = factory('App\Role')->create();
+        $permission = factory('App\Permission')->create();
+        $role->allowTo($permission);
+        $this->assertDatabaseHas('role_permissions', ['role_id' => $role->id, 'permission_id' => $permission->id]);
     }
 
     /** @test */
