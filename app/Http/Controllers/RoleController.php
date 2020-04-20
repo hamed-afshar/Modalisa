605c3de97fdbd61832255e2c52edac6fca6be269
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-21 00:26:10 +0430

    Update RoleController.php

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\DB;
 
 class RoleController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Role::class, 'role');
+    }
+
     //index roles
     public function index()
     {
