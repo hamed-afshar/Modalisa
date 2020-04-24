d9a822084645716c9be7c891f091912d1c482b85
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-25 01:26:54 +0430

    Create PermissionPolicy.php

---
+++ app/Policies/PermissionPolicy.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Policies;
+
+use App\Permission;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class PermissionPolicy
+{
+    use HandlesAuthorization;
+
+    public function before(User $user)
+    {
+        //dd("before permission");
+        if(!($user->isAdmin()) || $user->isLocked() || !($user->isConfirmed())) {
+            return false;
+        }
+    }
+
+    /**
+     * Determine whether the user can view any permissions.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the permission.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Permission  $permission
+     * @return mixed
+     */
+    public function view(User $user, Permission $permission)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can create permissions.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the permission.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Permission  $permission
+     * @return mixed
+     */
+    public function update(User $user, Permission $permission)
+    {
+        dd("$permission");
+    }
+
+    /**
+     * Determine whether the user can delete the permission.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Permission  $permission
+     * @return mixed
+     */
+    public function delete(User $user, Permission $permission)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the permission.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Permission  $permission
+     * @return mixed
+     */
+    public function restore(User $user, Permission $permission)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the permission.
+     *
+     * @param  \App\User  $user
+     * @param  \App\Permission  $permission
+     * @return mixed
+     */
+    public function forceDelete(User $user, Permission $permission)
+    {
+        //
+    }
+}
