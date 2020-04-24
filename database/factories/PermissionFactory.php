1f2d72a950f6de545cb445b77267f3d2c80d2575
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-25 01:27:23 +0430

    Update PermissionFactory.php

--- database/factories/PermissionFactory.php
+++ database/factories/PermissionFactory.php
@@ -8,6 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Permission::class, function (Faker $faker) {
     return [
         //'name' => $faker->randomElement($array = array('create-role', 'create-user', 'assign-role'))
-        'name' => 'create-role'
+        'name' => 'create-role',
+        'label' => 'create'
     ];
 });
