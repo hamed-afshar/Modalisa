653921298b2a1e83cc13b57fc377edc24a084da0
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-25 01:26:35 +0430

    Update PermissionController.php

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -7,9 +7,20 @@ use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
+
     //index permissions
+    /**
+     * PermissionController constructor.
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Permission::class, 'permission');
+    }
+
     public function index()
     {
+        dd("index");
+        $this->authorize('viewAny', auth()->user());
         $permissions = Permission::all();
         return view('permissions.index', compact('permissions'));
     }
@@ -17,32 +28,40 @@ class PermissionController extends Controller
     //form to create permissions
     public function create()
     {
+        dd("create");
+        $this->authorize('create', Permission::class);
         return view('permissions.create');
     }
 
     //store permissions
     public function store()
     {
+        dd("store");
+        $this->authorize('create', Permission::class);
         Permission::create(request()->validate([
-            'name' => 'required'
+            'name' => 'required',
+            'label' => 'required'
         ]));
     }
 
     //show a single permission
     public function show(Permission $permission)
     {
+        $this->authorize('view', auth()->user(), $permission);
         return view('permissions.show', compact('permission'));
     }
 
     //edit form
     public function edit(Permission $permission)
     {
+        $this->authorize('update', auth()->user(), $permission);
         return view('permissions.edit', compact('permission'));
     }
 
     //update permission
     public function update(Permission $permission)
     {
+        dd("update");
         $data = request()->validate([
            'name' => 'required'
         ]);
@@ -50,9 +69,9 @@ class PermissionController extends Controller
     }
 
     //delete a permission
-    //delete a role
     public function destroy(Permission $permission)
     {
+        dd("delete");
         $permission->delete();
     }
 }
