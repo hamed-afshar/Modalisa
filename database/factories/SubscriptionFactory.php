879022f443d854c2b39f6bc7c9465747a89b2269
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-26 19:59:47 +0430

    Update SubscriptionFactory.php

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Subscription::class, function (Faker $faker) {
     return [
-        'plan' => $faker->randomElement($array = array('Gold', 'Silver', 'Bronze')),
+        'plan' => $faker->unique()->randomElement($array = array('Gold', 'Silver', 'Bronze', 'test')),
         'cost_percentage' => $faker->randomElement($array = array(0, 10, 20, 30))
     ];
 });
