8dc458ff4265d1b285fa82ec4c83cf536e87646a
hamed-afshar <hamed.afshar.n@gmail.com>
2020-04-29 17:14:11 +0430

    Update index.blade.php

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,25 +1,57 @@
-@extends ('layouts.app')
-        <!DOCTYPE HTML>
-<html>
-@section('content')
-    <h1> All Users </h1>
-    <ul>
-        @forelse($users as $user)
-            <li>
-                <h2> {{$user->name}} </h2>
-                <h2> {{$user->email}} </h2>
-                <h2> {{$user->confirmed}} </h2>
-                <h2> {{$user->last_login}} </h2>
-                <h2> {{$user->lock}} </h2>
-                <h2> {{$user->last_ip}} </h2>
-                <h2> {{$user->language}} </h2>
-                <h2> {{$user->tel}} </h2>
-                <h2> {{$user->country}} </h2>
-                <h2> {{$user->communication_media}} </h2>
-            </li>
-        @empty
-            <li> Not any registerd user yet</li>
-        @endforelse
-    </ul>
+@extends('dashboards._admin_header')
+@section('index-content')
+    <div class="mt-4">
+        <div class="container mx-auto">
+            <div class="grid grid-rows-1">
+                <table class="table-auto mb-8">
+                    <thead>
+                    <tr class="bg-pink-600 text-white">
+                        <th class="border px-4 py-2"> ID</th>
+                        <th class="border px-4 py-2"> Name</th>
+                        <th class="border px-4 py-2"> Email</th>
+                        <th class="border px-4 py-2"> Subscription</th>
+                        <th class="border px-4 py-2"> Confirmed</th>
+                        <th class="border px-4 py-2"> Locked</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @forelse($users as $user)
+                        <tr class="bg-white">
+                            <td class="border text-center px-4 py-2"> {{$user->id}} </td>
+                            <td class="border px-4 py-2"> {{$user->name}} </td>
+                            <td class="border px-4 py-2"> {{$user->email}} </td>
+                            <td class="border px-4 py-2">
+                                <select class="input-option w-full" id="subscription" name="subscription">
+                                    <option selected disabled> {{$user->subscription->plan}} </option>
+                                    <option value="Gold"> Gold </option>
+                                    <option value="Silver"> Silver </option>
+                                    <option value="Bronze"> Bronze </option>
+                                </select>
+
+                            </td>
+                            <td class="border text-center px-4 py-2">
+                                @if($user->confirmed == true)
+                                    <i class="fas fa-check text-green-600"></i>
+                                @else
+                                    <i class="fas fa-ban text-red-600"></i>
+                                @endif
+                            </td>
+                            <td class="border text-center px-4 py-2">
+                                @if($user->locked == true)
+                                    <i class="fas fa-ban text-red-600"></i>
+                                @else
+                                    <i class="fas fa-check text-green-600"></i>
+                                @endif
+                            </td>
+
+                        </tr>
+                    @empty
+                        <p> User's table is empty</p>
+                    @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
 
