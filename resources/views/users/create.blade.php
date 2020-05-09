ad5b9a15195eada3ce71663b723f7d3a59e65f15
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-10 00:57:45 +0430

    Update create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,11 +1,11 @@
 @include('_site_header')
-<div class="container max-w-xs md:max-w-full mx-auto mt-16">
-    <div class="grid grid-cols-1 md:grid-cols-6 grid-rows-6">
-        <div class="col-start-3 col-span-2 row-start-2 row-end-5">
+<div class="container max-w-xs md:max-w-full mx-auto mt-2 mb-6">
+    <div class="grid grid-cols-1 md:grid-cols-6">
+        <div class="col-start-3 col-span-2">
             <div class="flex flex-wrap justify-center">
                 <div
                         class="inline-block bg-pink-800 border border-solid rounded-full px-8 py-4 transform translate-y-1/2 shadow-xl text-white font-bold">
-                    Register
+                    {{ __("translate.signup") }}
                 </div>
             </div>
             <form method="POST" action="{{ route('users.store')}}" class="bg-white shadow-md rounded px-8 py-8">
@@ -100,7 +100,7 @@
                 </div>
                 <div class="flex flex-wrap mb-6">
                     <div class="w-full lg:w-1/3">
-                        <label class="label mb-1 py-1" for="password"> {{  __('Password')  }} </label>
+                        <label class="label mb-1 py-1" for="password"> {{  __('translate.password')  }} </label>
                     </div>
                     <div class="w-full lg:w-2/3">
                         <input class="input-text w-full" type="password" id="password" name="password"
@@ -112,7 +112,7 @@
                 </div>
                 <div class="flex flex-wrap mb-6">
                     <div class="w-full lg:w-1/3">
-                        <label class="label mb-1 py-1" for="password_confirmation"> {{  __('Password Confirm')  }} </label>
+                        <label class="label mb-1 py-1" for="password_confirmation"> {{  __('translate.password_confirm')  }} </label>
                     </div>
                     <div class="w-full lg:w-2/3">
                         <input class="input-text w-full" type="password" id="password_confirmation" name="password_confirmation"
@@ -130,7 +130,7 @@
                     <div class="label pt-2">{{ __("translate.already_have_an_account?") }}</div>
                 </div>
                 <div class="flex justify-center w-full">
-                    <a href="#" class="text-blue-400 font-bold hover:text-blue-600 p-1">{{ __("translate.login") }}</a>
+                    <a href="#" class="link p-1">{{ __("translate.login") }}</a>
                 </div>
             </form>
         </div>
