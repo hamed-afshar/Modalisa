812e8854c5aac3a5b5886a4a2a7f871f4cdc4ea5
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-10 00:57:52 +0430

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,18 +6,18 @@
         </div>
         <div class="flex flex-wrap bg-white shadow-2xl w-full">
             <div class="w-full">
-                <h1 class="logo-large text-center text-"> Modalisa </h1>
+                <h1 class="logo-large text-center text-"> {{ __("translate.site_name") }} </h1>
                 <hr class="border-pink-600">
             </div>
             <div class="flex md:justify-end justify-center logo-small md:pr-5 py-2 md:py-0 w-full md:w-1/2">
-                <a href="#"
+                <a href=""
                    class="border-b-2 border-solid border-pink-600 md:border-transparent hover:text-pink-600 hover:border-pink-600">
-                    Login </a>
+                    {{ __("translate.login") }} </a>
             </div>
             <div class="flex md:justify-start justify-center logo-small  md:pl-3 py-2 md:py-0 w-full md:w-1/2">
-                <a href="#"
+                <a href="{{ route('users.create') }}"
                    class="border-b-2 border-solid border-pink-600 md:border-transparent hover:text-pink-600 hover:border-pink-600">
-                    Sign-up </a>
+                    {{ __("translate.signup") }} </a>
             </div>
         </div>
         <div class="bg-white flex shadow-2xl w-full">
