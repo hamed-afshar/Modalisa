bee5c986e77f621077dc41698c5beb771689b237
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-16 17:09:46 +0430

    Update show.blade.php
    for vue learning

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -8,9 +8,33 @@
 <body>
 <h1> Modalisa </h1>
 <ul>
-    {{ $role->name }}
+    {{--    {{ $role->name }}--}}
 </ul>
 
+<div id="app" v-model="name">
+   <ul>
+       <li v-for="name in names" v-text="name">
+           @{{ name }}
+       </li>
+   </ul>
+    <input id="input" type="text" v-model="newName">
+    <button v-on:click="addName"> add name </button>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/vue"></script>
+<script>
+    var app= new Vue({
+        el: '#app',
+        data: {
+            newName: '',
+            names: ["hamed", "amin", "ghazal"]
+        },
+        methods: {
+            addName() {
+                this.names.push(this.newName);
+            }
+        }
+    })
+</script>
 </body>
 </html>
 
