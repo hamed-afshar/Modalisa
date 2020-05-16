52cdb9393a6624ca99385569817921a8ce51160a
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-17 00:22:56 +0430

    Update show.blade.php

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -12,28 +12,36 @@
 </ul>
 
 <div id="app" v-model="name">
-   <ul>
-       <li v-for="name in names" v-text="name">
-           @{{ name }}
-       </li>
-   </ul>
-    <input id="input" type="text" v-model="newName">
-    <button v-on:click="addName"> add name </button>
+    <ul>
+        <li>
+            <task> Go to School</task>
+            <task> Go to Work</task>
+            <task> Go Home</task>
+        </li>
+    </ul>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/vue"></script>
 <script>
-    var app= new Vue({
-        el: '#app',
-        data: {
-            newName: '',
-            names: ["hamed", "amin", "ghazal"]
-        },
-        methods: {
-            addName() {
-                this.names.push(this.newName);
+    vue.component('task-list', {
+        'template: <div> <task v-for="student in students"> @{{student.name}} </task> </div>',
+        data() {
+            return {
+                students: [
+                    {name: "hamed", graduated: true}
+                    {name: "amin", graduated: true}
+                    {name: "ghazal", graduated: true}
+                    {name: "shadi", graduated: true}
+                ]
             }
         }
     })
+    Vue.component('task', {
+        template: '<li><slot></slot></li>'
+    })
+
+    new Vue({
+        el: '#app'
+    });
 </script>
 </body>
 </html>
