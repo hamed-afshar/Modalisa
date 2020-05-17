a71379e587c1f5ebfa1e475b728d5481249219ec
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-17 15:25:54 +0430

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,7 +23,7 @@ Vue.use(VModal);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('modalisa-msg', require('./components/ExampleComponent.vue').default);
 Vue.component('AddRoleModal', require('./components/AddRoleModal').default);
 
 /**
