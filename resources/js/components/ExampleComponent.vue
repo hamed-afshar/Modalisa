6fbf87a1c2932d6f053cb7116becf3cc79e05313
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-18 17:05:51 +0430

    Update ExampleComponent.vue

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,23 +1,37 @@
 <template>
-	<div name="modalisa-msg">
-		<div class="text-red-600" v-show="isVisible"><h1> {{ title }}</h1></div>
-		<div class="text-green-600"><h1> {{ body }} </h1></div>
-		<button class="btn-pink" v-on:click="hideTitle"> Hide</button>
-	</div>
+	<modal name="modalisa-modal" id="modalisa-modal" height="auto">
+		<div class="box h-48">
+			<div class="box-header">
+				<div class="flex flex-row">
+					<div class="w-1/2">
+						<h1 class="font-bold"> Add New Role </h1>
+					</div>
+					<div class="w-1/2 flex pt-1 justify-end">
+						<button @click="$emit('close')"></button>
+					</div>
+				</div>
+			</div>
+			<div class="grid grid-cols-6 mt-6">
+				<div class="col-start-3 col-end-5">
+					<div class="flex">
+						<input class="input-text w-full" type="text" id="newRoleName" name="newRoleName"
+						       placeholder="New Role Name" autofocus>
+					</div>
+					<div class="flex w-full mt-2">
+						<button class="btn-pink w-full"> Save</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</modal>
 </template>
 
 <script>
     export default {
-        name: "ExampleComponent",
-        props: ["title", "body"],
-        methods: {
-            hideTitle() {
-				this.isVisible=false
-            },
-        },
         data() {
             return {
-                isVisible: true
+                showModal: false
             }
         }
     }
