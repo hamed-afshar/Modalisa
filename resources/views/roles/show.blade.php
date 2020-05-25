5396c6cbe7319d82defeb462fd182895a05a8730
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-26 01:05:39 +0430

    update views

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,33 +1,8 @@
 @extends('dashboards._admin_header')
 @section('index-content')
-<h1> Modalisa </h1>
-<ul>
-    {{--    {{ $role->name }}--}}
-</ul>
-
-<div id="app">
-    <modalisa-modal v-show="showModal" @close="showModal = false"></modalisa-modal>
-    <button class="btn-green py-2" v-on:click="showModal = true"> Show Modal</button>
-</div>
-{{--<script src="https://cdn.jsdelivr.net/npm/vue"></script>--}}
-{{--<script>--}}
-{{--    // vue.component('modalisa-msg', {--}}
-{{--    //     props: ['title', 'body'],--}}
-{{--    //     template:--}}
-{{--    //         `--}}
-{{--    //        <div class="text-red-500">--}}
-{{--    //                 <h1> This is title</h1>--}}
-{{--    //                 @{{title}}--}}
-{{--    //             </div>--}}
-{{--    //             <div class="text-green-600">--}}
-{{--    //                 <h3> this is body</h3>--}}
-{{--    //                 @{{ body }}--}}
-{{--    //             </div>--}}
-{{--    //       `--}}
-{{--    // })--}}
-{{--    //--}}
-{{--    // new Vue({--}}
-{{--    //     el: '#app'--}}
-{{--    // });--}}
-{{--</script>--}}
+    <div id="app">
+        <customized-table v-bind:columns ="['id','name','date']"></customized-table>
+    </div>
 @endsection
+
+
