2df11b016126dbbb0f86d5b33d402185aed2e93e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-26 17:25:36 +0430

    Update show.blade.php

--- resources/views/roles/show.blade.php
+++ resources/views/roles/show.blade.php
@@ -1,8 +1,6 @@
 @extends('dashboards._admin_header')
 @section('index-content')
-    <div id="app">
-        <customized-table v-bind:columns ="['id','name','date']"></customized-table>
-    </div>
+  test
 @endsection
 
 
