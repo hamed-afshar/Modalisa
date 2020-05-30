a70b4839e3ff0ae43e778b7263e5257858ca2576
hamed-afshar <hamed.afshar.n@gmail.com>
2020-05-30 15:49:32 +0430

    Delete messages.js

--- resources/messages.js
+++
@@ -1,6 +0,0 @@
-// messages.js
-export default {
-    // The key format should be: 'locale.filename'.
-    'en.messages': require('../../resources/lang/en/messages.php'),
-    'en.auth': require('./lang/en/auth.php'),
-}
