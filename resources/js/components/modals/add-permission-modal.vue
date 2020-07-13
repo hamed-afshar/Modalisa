557bc57fb9eb60f964ad6703d9f9d41309228a5e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-07-13 17:12:24 +0430

    Update permission modal

---
+++ resources/js/components/modals/add-permission-modal.vue
@@ -0,0 +1,89 @@
+<template>
+	<modal name="add-permission-modal" id="add-permission-modal" height="auto">
+		<div class="modal-box h-64">
+			<div class="modal-header">
+				<div class="flex flex-row">
+					<div class="w-1/2">
+						<h1 class="font-bold"> {{ fields.title }} </h1>
+					</div>
+					<div class="w-1/2 flex pt-1 justify-end">
+						<i class="fas fa-times cursor-pointer" v-on:click="$modal.hide('add-permission-modal')"></i>
+					</div>
+				</div>
+			</div>
+			<div class="grid grid-cols-6 mt-3">
+				<div class="col-start-3 col-end-5">
+					<div>
+						<div class="flex mt-2">
+							<input class="input-text w-full" type="text" v-model="permissionName" id="permissionName" name="permissionName"
+							       placeholder="Name" autofocus>
+						</div>
+						<div class="flex mt-2">
+							<input class="input-text w-full" type="text" v-model="permissionLabel" id="permissionLabel" name="permissionLabel"
+							       placeholder="Label1" autofocus>
+						</div>
+					</div>
+					<div class="flex w-full mt-4">
+						<button class="btn-pink w-full mb-2" v-on:click="save"> {{ $t('translate.save') }} </button>
+					</div>
+					<div>
+						{{ errors.get('name')}}
+						{{ errors.get('label')}}
+					</div>
+				</div>
+			</div>
+		</div>
+	</modal>
+</template>
+
+<script>
+	class Errors {
+		constructor() {
+		    this.errors = {}
+        }
+
+        get(field) {
+		    if(this.errors[field] ) {
+		        return this.errors[field][0];
+		    }
+        }
+
+        record(errors) {
+		    this.errors = errors.errors
+        }
+	}
+    export default {
+        name: "add-permission-modal",
+        props: {
+            fields: Object
+        },
+        data() {
+            return {
+	            permissionName: null,
+	            permissionLabel: null,
+	            errors: new Errors()
+            }
+        },
+	    methods: {
+            save() {
+				axios.post('/permissions', {
+				    name: this.permissionName,
+					label: this.permissionLabel
+	            }).then(function() {
+	                Event.$emit('save');
+				}).catch(error => this.errors.record(error.response.data))
+            }
+	    },
+    }
+</script>
+
+<style scoped>
+	.modal-box {
+		@apply .bg-white .text-gray-500 .border .rounded-lg .p-2;
+	}
+
+	.modal-header {
+		@apply .inline-block .border .rounded .shadow-2xl .bg-purple-700 .px-3 .py-2 .w-full .text-white .font-bold;
+	}
+
+</style>
--- resources/js/components/tables/permissions-table.vue
+++ resources/js/components/tables/permissions-table.vue
@@ -1,40 +1,34 @@
 <template>
-    <div class="flex flex-col">
-        <table class="table-auto mb-2">
-            <thead class="sticky top-0">
-            <tr class="table-header-row">
-                <th class="table-header-cell" v-show="columns.length" v-for="column in columns" :key="column">
-                    {{ column }}
-                </th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr class="table-body-row" v-for="permission in permissions" v-bind:key="permission.id">
-                <td class="table-body-cell">
-                    <a class="link" v-bind:href="path + permission.id"> {{ permission.name }} </a>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-        <div class="flex justify-end transform -translate-y-6 translate-x-5">
-            <button v-on:click="openModal">
-                <circle-plus-button></circle-plus-button>
-            </button>
-        </div>
-        <general-modal
-            v-bind:fields="{
+	<div class="flex flex-col">
+		<table class="table-auto mb-2">
+			<thead class="sticky top-0">
+			<tr class="table-header-row">
+				<th class="table-header-cell" v-show="columns.length" v-for="column in columns" :key="column">
+					{{ column }}
+				</th>
+			</tr>
+			</thead>
+			<tbody>
+			<tr class="table-body-row" v-for="permission in permissions" v-bind:key="permission.id">
+				<td class="table-body-cell">
+					<a class="link" v-bind:href="path + permission.id"> {{ permission.name }} </a>
+				</td>
+			</tr>
+			</tbody>
+		</table>
+		<div class="flex justify-end transform -translate-y-6 translate-x-5">
+			<button v-on:click="$modal.show('add-permission-modal')">
+				<circle-plus-button></circle-plus-button>
+			</button>
+		</div>
+		<add-permission-modal v-bind:fields="{
             title: 'Add a Permission',
             attr: {
-                inputs:[
-                        { name: 'name', label: 'name'},
-                        { name: 'label', label: 'label'}
-                ],
                 buttons:['Save'],
             },
-            route: this.route
         }">
-        </general-modal>
-    </div>
+		</add-permission-modal>
+	</div>
 </template>
 
 <script>
@@ -42,26 +36,29 @@
         name: "permission-table",
         props: {
             columns: Array,
-            route: {
-                type: String,
-                required: true
-            }
         },
         data() {
             return {
                 permissions: [],
                 path: '/permissions/',
-                modalOpen: false
             }
         },
         methods: {
-            openModal() {
-                this.$root.$emit('open-modal');
-            },
+	        //function to execute after saving permission in db
+            save() {
+                axios.get('./permissions')
+                    .then(response => this.permissions = response.data);
+                // close modal
+                this.$modal.hide('add-permission-modal')
+            }
         },
+
         mounted() {
             axios.get('./permissions')
                 .then(response => this.permissions = response.data);
+            Event.$on('save', () => {
+                this.save();
+            })
         }
     }
 </script>
