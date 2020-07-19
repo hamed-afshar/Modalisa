7af7782e654ed1a2db12cb8e8c196779bb9d7463
hamed-afshar <hamed.afshar.n@gmail.com>
2020-07-19 17:05:11 +0430

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -29,9 +29,12 @@ Vue.component('roles-table', require('./components/tables/roles-table').default)
 Vue.component('permission-table', require('./components/tables/permissions-table').default);
 Vue.component('circle-plus-button', require('./components/buttons/circle-plus-button').default);
 Vue.component('add-role-modal', require('./components/modals/add-role-modal').default);
+Vue.component('delete-role-modal', require('./components/modals/delete-role-modal').default);
+
 Vue.component('add-permission-modal', require('./components/modals/add-permission-modal').default);
 Vue.component('delete-permission-modal', require('./components/modals/delete-permission-modal').default);
-Vue.component('delete-role-modal', require('./components/modals/delete-role-modal').default);
+Vue.component('edit-permission-modal', require('./components/modals/edit-permission-modal').default);
+
 
 
 
