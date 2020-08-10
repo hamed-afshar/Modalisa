ac6ae4ecfa69983c296df002781086250de601ff
hamed-afshar <hamed.afshar.n@gmail.com>
2020-08-10 10:42:14 +0430

    Update grant-permission-modal.vue

--- resources/js/components/modals/grant-permission-modal.vue
+++ resources/js/components/modals/grant-permission-modal.vue
@@ -12,65 +12,62 @@
 					</div>
 				</div>
 			</div>
-			<div class="flex flex-col overflow-y-auto h-48">
+			<div class="flex flex-col">
 				<table class="table-auto mb-2">
 					<thead>
-					<tr class="">
-						<th class="table-header-cell">
+					<tr>
+						<th class="table-header-row">
 							{{ $t('translate.grant_permission')}}
 						</th>
 					</tr>
 					</thead>
 					<tbody>
-					<tr class="table-body-row" v-for="permission in associatedPermissions" v-bind:key="permission.id">
-						<td class="table-body-cell">
-							<div class="flex flex-row">
-								<div class="w-4/12">
-									{{ permission.name }}
-								</div>
-								<div class="w-7/12">
-									{{ permission.label }}
-								</div>
-								<div class="w-1/12 flex flex-row">
-									<i class="fas fa-minus minus-button cursor-pointer" v-on:click=""></i>
-								</div>
-							</div>
-						</td>
-					</tr>
+					<div class="overflow-y-auto h-48">
+						<tr class="table-body-row flex w-full" v-for="permission in associatedPermissions"
+						    v-bind:key="permission.id">
+							<td class="w-4/12">
+								{{ permission.name }}
+							</td>
+							<td class="w-7/12">
+								{{ permission.label }}
+							</td>
+							<td class="w-1/12">
+								<i class="fas fa-minus minus-button cursor-pointer" v-on:click=""></i>
+							</td>
+						</tr>
+					</div>
 					</tbody>
 				</table>
 			</div>
-			<div class="flex flex-col overflow-y-auto h-48 mt-2">
+			<div class="flex flex-col">
 				<table class="table-auto mb-2">
 					<thead>
-					<tr class="">
-						<th class="table-header-cell">
+					<tr>
+						<th class="table-header-row">
 							{{ $t('translate.all_permissions')}}
 						</th>
 					</tr>
 					</thead>
 					<tbody>
-					<tr class="table-body-row" v-for="permission in allPermissions" v-bind:key="permission.id">
-						<td class="table-body-cell">
-							<div class="flex flex-row">
-								<div class="w-4/12">
-									{{ permission.name }}
-								</div>
-								<div class="w-7/12">
-									{{ permission.label }}
-								</div>
-								<div class="w-1/12 flex flex-row">
-									<i class="fas fa-plus plus-button cursor-pointer" v-on:click=""></i>
-								</div>
-							</div>
-						</td>
-					</tr>
+					<div class="overflow-y-auto h-48">
+						<tr class="table-body-row flex w-full" v-for="permission in allPermissions"
+						    v-bind:key="permission.id">
+							<td class="w-4/12">
+								{{ permission.name }}
+							</td>
+							<td class="w-7/12">
+								{{ permission.label }}
+							</td>
+							<td class="w-1/12">
+								<i class="fas fa-plus plus-button cursor-pointer" v-on:click=""></i>
+							</td>
+						</tr>
+					</div>
 					</tbody>
 				</table>
 			</div>
 		</div>
 	</modal>
-
 </template>
 
 <script>
@@ -116,23 +113,15 @@
 	}
 
 	.table-header-row {
-		@apply .bg-purple-700 .text-white
-	}
-
-	.table-header-cell {
-		@apply .border .px-4 .py-2 .text-center
+		@apply .bg-white .text-gray-600 .px-4 .py-2
 	}
 
 	.table-body-row {
-		@apply .bg-white
+		@apply .bg-white .border .px-4 .py-2 .text-center .text-gray-800
 	}
 
-	.table-body-cell {
-		@apply .border .px-4 .py-2 .text-center .text-gray-800
+	.table-body-row:hover {
+		@apply .bg-gray-200
 	}
 
-	.th {
-		position: sticky;
-		top: 0;
-	}
 </style>
