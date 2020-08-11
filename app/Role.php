4d48e1aceda46a554d47f02cdd27f88d1e5372d7
hamed-afshar <hamed.afshar.n@gmail.com>
2020-08-11 17:12:11 +0430

    Update Role.php

--- app/Role.php
+++ app/Role.php
@@ -8,25 +8,41 @@ class Role extends Model
 {
     protected $guarded = [];
 
-    //Role might belongs to many users
+    /*
+     * Role might belongs to many users
+     */
     public function users()
     {
         return $this->belongsToMany('App\User', 'user_roles')->withTimestamps();
     }
 
-    //Role might belongs to many permissions
+    /*
+     * Role might belongs to many permissions
+     */
     public function permissions()
     {
         return $this->belongsToMany('App\Permission', 'role_permissions')->withTimestamps();
     }
 
-    //assign permission to role
+    /*
+     * allow permission for roles
+     */
     public function allowTo($permission)
     {
         $this->permissions()->sync($permission, false);
     }
 
-    //return role path
+    /*
+     * disallow permissions for roles
+     */
+    public function  disAllowTo($permission)
+    {
+        $this->permissions()->detach($permission);
+    }
+
+    /*
+     * return role path
+     */
     public function path()
     {
         return "/roles/{$this->id}";
