1788ab3f28a4da99c6e2781fae6c0a7a109a1db0
hamed-afshar <hamed.afshar.n@gmail.com>
2020-08-12 16:29:10 +0430

    Update security-center.blade.php
    Make it responsive

--- resources/views/dashboards/security-center.blade.php
+++ resources/views/dashboards/security-center.blade.php
@@ -1,21 +1,15 @@
 @extends('dashboards._admin_header')
 @section('index-content')
-    <div id="app" class="grid grid-cols-6">
+    <div id="app" class="flex flex-col md:flex-col lg:flex-row">
         <!-- left side -->
-        <div class="grid col-span-3 mt-2">
-            <div class="flex flex-col">
-                <div class="box-header"> {{ __("translate.role") }}</div>
-                <roles-table></roles-table>
-            </div>
+        <div class="lg:w-1/2">
+            <div class="box-header w-full"> {{ __("translate.role") }}</div>
+            <roles-table></roles-table>
         </div>
         <!-- right side -->
-        <div class="grid col-span-3 mt-2 ml-10">
-            <div class="grid grid-rows-2 grid-flow-col">
-                <div class="flex flex-col">
-                    <div class="box-header"> {{ __("translate.permission") }}</div>
-                    <permission-table></permission-table>
-                </div>
-            </div>
+        <div class="lg:w-1/2 lg:ml-10">
+            <div class="box-header w-full"> {{ __("translate.permission") }}</div>
+            <permission-table></permission-table>
         </div>
     </div>
 @endsection
