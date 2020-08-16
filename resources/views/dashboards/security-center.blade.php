b70a817947b2d78ac6c91a3f9c17c8541dd76fd5
hamed-afshar <hamed.afshar.n@gmail.com>
2020-08-16 16:32:20 +0430

    Update security-center.blade.php

--- resources/views/dashboards/security-center.blade.php
+++ resources/views/dashboards/security-center.blade.php
@@ -1,16 +1,23 @@
 @extends('dashboards._admin_header')
 @section('index-content')
     <div id="app" class="flex flex-col md:flex-col lg:flex-row">
-        <!-- left side -->
-        <div class="lg:w-1/2">
-            <div class="box-header w-full"> {{ __("translate.role") }}</div>
+        <!-- left side roles -->
+        <div class="lg:w-2/5">
+            <div class="box-header w-full"> {{ trans_choice("translate.role" , 2) }} </div>
             <roles-table></roles-table>
         </div>
-        <!-- right side -->
-        <div class="lg:w-1/2 lg:ml-10">
-            <div class="box-header w-full"> {{ __("translate.permission") }}</div>
+        <!-- center permissions-->
+        <div class="lg:w-2/5 lg:ml-10">
+            <div class="box-header w-full"> {{ trans_choice("translate.permission" , 2) }} </div>
             <permission-table></permission-table>
         </div>
+        <!-- right side subscriptions-->
+        <div class="lg:w-1/5 lg:ml-10">
+            <div class="box-header w-full"> {{ trans_choice("translate.subscription" , 2) }}</div>
+            <subscriptions-table> </subscriptions-table>
+        </div>
+
+
     </div>
 @endsection
 
