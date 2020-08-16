5ff9b644adeaba517c27539b5373b9b42694889b
hamed-afshar <hamed.afshar.n@gmail.com>
2020-08-16 16:32:12 +0430

    Create SubscriptionTest.php

---
+++ tests/Unit/SubscriptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+
+class SubscriptionTest extends TestCase
+{
+    use WithFaker,
+        RefreshDatabase;
+    /** @test */
+    public function get_user_subscription ()
+    {
+        $this->withourE
+
+    }
+}
