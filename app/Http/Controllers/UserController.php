0cc270afcf8d7ec913099f83c613e63d3d8a25ed
hamed-afshar <hamed.afshar.n@gmail.com>
2020-09-01 09:21:39 +0430

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,8 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $this->authorize('viewAny', User::class);
-        $users =  User::with('subscription')->get();
-        return json_decode($users);
+        return User::with('subscription')->get();
     }
 
     //systemadmin can see a single user
