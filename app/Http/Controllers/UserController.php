fb7675549f315acd3d2bce26dbc8d2c014176a47
hamed-afshar <hamed.afshar.n@gmail.com>
2020-09-01 17:06:14 +0430

    Update UserController.php
    return user with subscription and role assosicated with

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $this->authorize('viewAny', User::class);
-        return User::with('subscription')->get();
+        return User::with(['subscription', 'role'])->get();
     }
 
     //systemadmin can see a single user
