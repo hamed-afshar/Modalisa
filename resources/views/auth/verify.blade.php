5cfe10e18a078325a923f352e5e98b48c77bfb39
hamed-afshar <hamed.afshar.n@gmail.com>
2020-09-07 21:06:59 +0430

    Update verify.blade.php

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -31,42 +31,14 @@
                             <div class="flex justify-center">
                                 @if(session('resent'))
                                     <div class="success">
-                                        {{ __('translate.verify_again') }}
+                                        {{ __('translate.request_sent') }}
                                     </div>
                                 @endif
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-        {{--        <div class="container">--}}
-        {{--            <div class="row justify-content-center">--}}
-        {{--                <div class="col-md-8">--}}
-        {{--                    <div class="card">--}}
-        {{--                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>--}}
-
-        {{--                        <div class="card-body">--}}
-        {{--                            @if (session('resent'))--}}
-        {{--                                <div class="alert alert-success" role="alert">--}}
-        {{--                                    {{ __('A fresh verification link has been sent to your email address.') }}--}}
-        {{--                                </div>--}}
-        {{--                            @endif--}}
-
-        {{--                            {{ __('Before proceeding, please check your email for a verification link.') }}--}}
-        {{--                            {{ __('If you did not receive the email') }},--}}
-        {{--                            <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">--}}
-        {{--                                @csrf--}}
-        {{--                                <button type="submit"--}}
-        {{--                                        class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>--}}
-        {{--                                .--}}
-        {{--                            </form>--}}
-        {{--                        </div>--}}
-        {{--                    </div>--}}
-        {{--                </div>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--    </div>--}}
     </div>
 @endsection
