273d811c5e34305cf0aaed526403b5eb6a21bcba
hamed-afshar <hamed.afshar.n@gmail.com>
2020-09-07 23:50:05 +0430

    Create general-navbar.vue

---
+++ resources/js/components/navigation/general-navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <header class="flex items-center justify-between px-4 py-2 bg-white">
+        <!--Left Side -->
+        <div class="w-2/3">
+            <a v-bind:href="route('')">
+                <h1 class="logo-medium"> Modalisa </h1>
+            </a>
+        </div>
+        <div class="w-1/3">
+
+        </div>
+    </header>
+
+</template>
+
+<script>
+    export default {
+        name: "general-navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
