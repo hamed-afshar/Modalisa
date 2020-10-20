4b9468fa715f895efeb186616a660897017fb21c
hamed-afshar <hamed.afshar.n@gmail.com>
2020-10-20 16:22:39 +0330

    prepare normal user environment for test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -10,6 +10,10 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    /*
+     * prepare administrator environment
+     */
+
     protected function prepAdminEnv($role, $locked, $confirmed)
     {
         factory('App\Subscription')->create([
@@ -22,15 +26,31 @@ abstract class TestCase extends BaseTestCase
         $this->actingAs($user);
     }
 
+    /*
+     * prepare normal user environment
+     */
+
     protected function prepNormalEnv($role, $permission, $locked, $confirmed)
     {
-        {
-            $user = factory('App\User')->create(['confirmed' => $confirmed, 'locked' => $locked]);
-            $role = factory('App\Role')->create(['name' => $role]);
-            $permission = factory('App\Permission')->create(['name' => $permission]);
-            $user->assignRole($role);
-            $role->allowTo($permission);
-            $this->actingAs($user);
-        }
+        $user = factory('App\User')->create(['confirmed' => $confirmed, 'locked' => $locked]);
+        $role = factory('App\Role')->create(['name' => $role]);
+        $permission = factory('App\Permission')->create(['name' => $permission]);
+        $user->assignRole($role);
+        $role->allowTo($permission);
+        $this->actingAs($user);
+    }
+
+    /*
+     * prepare retailer environment
+     */
+
+    protected function prepRetailerEnv($role, $permission, $locked, $confirmed)
+    {
+        $user = factory('App\User')->create(['confirmed' => $confirmed, 'locked' => $locked]);
+        $role = factory('App\Role')->create(['name' => $role]);
+        $permission = factory('App\Permission')->create(['name' => $permission]);
+        $user->assignRole($role);
+        $role->allowTo($permission);
+        $this->actingAs($user);
     }
 }
