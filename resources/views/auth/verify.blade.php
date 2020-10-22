11cc299470d521f927ebc71a1a8aa3b4af1bc39c
hamed-afshar <hamed.afshar.n@gmail.com>
2020-10-22 22:15:19 +0330

    add logout function to verify page

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -6,8 +6,14 @@
                 <div class="box py-8 px-6 shadow-2xl">
                     <div class="flex flex-wrap">
                         <div class="w-full flex justify-end">
-                            <a href="{{ auth()->logout() }}"> <i class="fas fa-times cursor-pointer"></i> </a>
+                            <a href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <i class="fas fa-times cursor-pointer"></i> </a>
                         </div>
+                        <!-- logout form is needed for x button-->
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                         <div class="w-full text-center font-bold text-lg">
                             {{ __('translate.verify_email') }}
                         </div>
