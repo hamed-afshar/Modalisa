fedb7a0c56b2df887b2a79726631c9af2a12d114
hamed-afshar <hamed.afshar.n@gmail.com>
2020-10-28 17:09:16 +0330

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,18 @@
 @section('content')
     <!-- Dashboard for normal users -->
     <div id="app">
-        @guest
-            <guest-navbar></guest-navbar>
-        @endguest
-        @auth
-            <general-navbar></general-navbar>
-        @endauth
-        <users-sidebar></users-sidebar>
+        <div class="flex h-screen bg-gray-200">
+            <user-sidebar></user-sidebar>
+            <div class="flex-1 flex flex-col overflow-hidden">
+                <general-navbar></general-navbar>
+                <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+                    <div class="container mx-auto px-6 py-8">
+                        <user-dashboard></user-dashboard>
+                    </div>
+                </main>
+
+            </div>
+        </div>
     </div>
 @endsection
 <script>
