f5c6a8a8247b451c150b3181052ffd9f1aa4e12a
hamed-afshar <hamed.afshar.n@gmail.com>
2020-10-28 17:09:01 +0330

    Create user-dashboard.vue

---
+++ resources/js/components/dashboards/user-dashboard.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <h3 class="text-gray-700 text-3xl font-medium"> Dashboard </h3>
+    <div class="mt-4">
+      <div class="flex flex-wrap -mx-6">
+        <!-- orders short report -->
+        <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+          <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+            <div class="p-3 rounded-full bg-indigo-600 opacity-75">
+              <i class="fas fa-shopping-bag fa-2x text-white"></i>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">1250</h4>
+              <div class="text-gray-500"> Monthly</div>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">400</h4>
+              <div class="text-gray-500"> Weekly</div>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">30</h4>
+              <div class="text-gray-500"> Daily</div>
+            </div>
+          </div>
+        </div>
+        <!-- money short report -->
+        <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+          <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+            <div class="p-3 rounded-full bg-pink-600 opacity-75">
+              <i class="fas fa-dollar-sign fa-2x text-white"></i>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">130,000</h4>
+              <div class="text-gray-500"> Monthly</div>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">1895</h4>
+              <div class="text-gray-500"> Weekly</div>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">750</h4>
+              <div class="text-gray-500"> Daily</div>
+            </div>
+          </div>
+        </div>
+        <!-- shipping report -->
+        <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+          <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
+            <div class="p-3 rounded-full bg-orange-600 opacity-75">
+              <i class="fas fa-shipping-fast fa-2x text-white"></i>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">250</h4>
+              <div class="text-gray-500"> Orders</div>
+            </div>
+            <div class="mx-5">
+              <h4 class="text-2xl font-semibold text-gray-700">25 kg</h4>
+              <div class="text-gray-500"> Weight</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col mt-8">
+      <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          <orders-table-user></orders-table-user>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "user-dashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
