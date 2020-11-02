8e20c9b871bbff1418f1b04b53907f9a15a1d04b
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-02 14:52:38 +0330

    Update wallet-table-user.vue

--- resources/js/components/tables/wallet-table-user.vue
+++ resources/js/components/tables/wallet-table-user.vue
@@ -1,31 +1,38 @@
 <template>
-<div class="table-back">
-  <table class="table-design">
-    <thead class="border-b">
-    <tr>
-      <th class="table-header-cell"> {{ '#' }}</th>
-      <th class="table-header-cell"> {{ $t('translate.id') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.amount') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.currency') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.date') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.time') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.picture') }}</th>
-      <th class="table-header-cell"> {{ $t('translate.description') }}</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr v-for="transaction in transactions" v-bind:key="transaction.id" class="table-body-row">
-      <td class="table-body-cell">1 </td>
-      <td class="table-body-cell">5000 </td>
-      <td class="table-body-cell">TL</td>
-      <td class="table-body-cell">2020 </td>
-      <td class="table-body-cell">11:20 </td>
-      <td class="table-body-cell">pic </td>
-      <td class="table-body-cell">my first transction for dear modalisa </td>
-    </tr>
-    </tbody>
-  </table>
-</div>
+  <div>
+    <div class="table-back">
+      <table class="table-design">
+        <thead class="border-b">
+        <tr>
+          <th class="table-header-cell"> {{ '#' }}</th>
+          <th class="table-header-cell"> {{ $t('translate.id') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.amount') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.currency') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.date') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.time') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.picture') }}</th>
+          <th class="table-header-cell"> {{ $t('translate.description') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="transaction in transactions" v-bind:key="transaction.id" class="table-body-row">
+          <td class="table-body-cell"> 1</td>
+          <td class="table-body-cell"> 3264</td>
+          <td class="table-body-cell">5000</td>
+          <td class="table-body-cell">TL</td>
+          <td class="table-body-cell">2020</td>
+          <td class="table-body-cell">11:20</td>
+          <td class="table-body-cell">pic</td>
+          <td class="table-body-cell">my first transction for dear modalisa</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="flex justify-center">
+      <button class="btn-pink px-4 py-2 block"> Add Payment </button>
+    </div>
+  </div>
+
 </template>
 
 <script>
