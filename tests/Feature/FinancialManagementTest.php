285dfdcd9278bdafa99ee60df0901c7760b75f09
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-03 20:01:04 +0330

    Update FinancialManagementTest.php

--- tests/Feature/FinancialManagementTest.php
+++ tests/Feature/FinancialManagementTest.php
@@ -10,13 +10,32 @@ class FinancialManagementTest extends TestCase
 {
     use WithFaker,
         RefreshDatabase;
-   /** @test */
+
+    /** @test */
     public function retailers_can_see_their_transactions()
     {
-        $this->withExceptionHandling();
         $this->prepNormalEnv('retailer', 'make-payment', '0', 1);
         $transaction = factory('App\Transactions')->create(['user_id' => auth()->user()->id]);
         $this->get('/transactions')->assertSee($transaction->comment)
             ->assertSee(200);
+    }
+
+    /*
+     * form is available in vuejs modal
+     */
+    /** @test */
+//    public function form_is_available_to_create_a_transaction()
+//    {
+//
+//    }
+
+    /** @test */
+    public function retailers_can_create_transaction()
+    {
+//        $this->withExceptionHandling();
+        $this->prepNormalEnv('retailer','make-payment', 0 ,1);
+        $attributes = factory('App\Transactions')->raw(['user_id' => auth()->user()->id]);
+        $this->post('/transactions', $attributes);
+        $this->assertDatabaseHas('transactions', $attributes);
     }
 }
