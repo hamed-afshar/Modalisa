e9d410d965f796a54d3ea5ab7d06af75d325cd1a
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-07 00:11:58 +0330

    Update FinancialManagementTest.php

--- tests/Feature/FinancialManagementTest.php
+++ tests/Feature/FinancialManagementTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class FinancialManagementTest extends TestCase
@@ -11,31 +12,38 @@ class FinancialManagementTest extends TestCase
     use WithFaker,
         RefreshDatabase;
 
-    /** @test */
-    public function retailers_can_see_their_transactions()
-    {
-        $this->prepNormalEnv('retailer', 'make-payment', '0', 1);
-        $transaction = factory('App\Transactions')->create(['user_id' => auth()->user()->id]);
-        $this->get('/transactions')->assertSee($transaction->comment)
-            ->assertSee(200);
-    }
+    /** @test
+     * this test is not show anything in blade files because
+     * transaction information will be available in vuejs
+     */
+//    public function retailers_can_see_their_transactions()
+//    {
+//        $this->withoutExceptionHandling();
+//        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+//        $transaction = factory('App\Transaction')->create(['user_id' => auth()->user()->id]);
+//        $this->get('/transactions')->assertSee($transaction->comment)
+//            ->assertSee(200);
+//    }
 
     /*
-     * form is available in vuejs modal
+     * create form availability should be tested in VueJs
      */
-    /** @test */
-//    public function form_is_available_to_create_a_transaction()
-//    {
-//
-//    }
 
     /** @test */
     public function retailers_can_create_transaction()
     {
-//        $this->withExceptionHandling();
-        $this->prepNormalEnv('retailer','make-payment', 0 ,1);
-        $attributes = factory('App\Transactions')->raw(['user_id' => auth()->user()->id]);
+        $this->withoutExceptionHandling();
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => Auth::user()->id]);
         $this->post('/transactions', $attributes);
         $this->assertDatabaseHas('transactions', $attributes);
+    }
+
+    /** @test */
+    public function user_id_is_required()
+    {
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => '']);
+        $this->post('/transactions', $attributes)->assertSessionHasErrors('user_id');
     }
 }
