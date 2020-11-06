282c83145e9966e8e899c884db878415f4fd0b96
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-07 00:11:23 +0330

    Update TransactionController.php

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -11,7 +12,32 @@ class TransactionController extends Controller
      */
     public function index()
     {
-        $this->authorize('viewAny', Transactions::class);
+        $this->authorize('viewAny', Transaction::class);
+    }
+
+    /*
+     * form to create transaction
+     * VueJS modal generates this form
+     */
+    public function create()
+    {
+        $this->authorize('create', Transaction::class);
+    }
+
+    /*
+     * store transactions
+     */
+    public function store()
+    {
+        $this->authorize('create', Transaction::class);
+        Transaction::create(request()->validate([
+            'user_id' => 'required',
+            'currency' => 'required',
+            'amount' => 'required',
+            'pic' => 'required',
+            'comment' => 'required'
+        ]));
+
     }
 
 }
