4031ff870fa07d8f848ba85db8a1e922a1969e74
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-07 17:29:34 +0330

    Update FinancialManagementTest.php

--- tests/Feature/FinancialManagementTest.php
+++ tests/Feature/FinancialManagementTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Transaction;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
@@ -26,7 +27,8 @@ class FinancialManagementTest extends TestCase
 //    }
 
     /*
-     * create form availability should be tested in VueJs
+     * form_is_available_to_create_a_transaction
+     * this should be tested in VueJs
      */
 
     /** @test */
@@ -46,4 +48,60 @@ class FinancialManagementTest extends TestCase
         $attributes = factory('App\Transaction')->raw(['user_id' => '']);
         $this->post('/transactions', $attributes)->assertSessionHasErrors('user_id');
     }
+
+    /** @test */
+    public function currency_is_required()
+    {
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => Auth::user()->id, 'currency' => '']);
+        $this->post('/transactions', $attributes)->assertSessionHasErrors('currency');
+    }
+
+    /** @test */
+    public function amount_is_required()
+    {
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => Auth::user()->id, 'amount' => '']);
+        $this->post('/transactions', $attributes)->assertSessionHasErrors('amount');
+    }
+
+    /** @test */
+    public function pic_is_required()
+    {
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => Auth::user()->id, 'pic' => '']);
+        $this->post('/transactions', $attributes)->assertSessionHasErrors('pic');
+    }
+
+    /** @test */
+    public function comment_is_required()
+    {
+        $this->prepNormalEnv('retailer', 'make-payment', 0, 1);
+        $attributes = factory('App\Transaction')->raw(['user_id' => Auth::user()->id, 'comment' => '']);
+        $this->post('/transactions', $attributes)->assertSessionHasNoErrors();
+    }
+
+    /*
+     * retailer_can_see_a_single_transaction
+     * this should be tested in VueJs
+     */
+
+    /** @test */
+    public function retailer_can_edit_a_transaction()
+    {
+        $this->withoutExceptionHandling();
+        $this->prepNormalEnv('retailer', 'make-payment', 0 , 1);
+        $transaction = factory('App\Transaction')->create(['user_id' => Auth::user()->id]);
+        $newAttributes = [
+            'currency' => 'USD',
+            'amount' => '9999',
+            'pic' => 'new_link',
+            'comment' => 'new comment'
+        ];
+        $this->patch($transaction->path(), $newAttributes);
+        $this->assertDatabaseHas('transactions', $newAttributes);
+    }
+
+
+
 }
