92412c0e2e396b5a4eaf93a4ad05c0f8f708a873
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-23 14:34:33 +0330

    Developing Product observer model

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Observers;
+
+use App\History;
+use App\Product;
+
+class ProductObserver
+{
+    /**
+     * Handle the product "created" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function created(Product $product)
+    {
+        History::create([
+            'product_id'=> $product->id,
+            'status_id' => 1
+        ]);
+    }
+
+    /**
+     * Handle the product "updated" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function updated(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "deleted" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function deleted(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "restored" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function restored(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Handle the product "force deleted" event.
+     *
+     * @param  \App\Product  $product
+     * @return void
+     */
+    public function forceDeleted(Product $product)
+    {
+        //
+    }
+}
