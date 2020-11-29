83181829a3e1393c68ad34d8c871b46abac3c18e
hamed-afshar <hamed.afshar.n@gmail.com>
2020-11-29 12:31:50 +0330

    Createing Kargo model and all dependencies

---
+++ database/factories/KargoFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Kargo;
+use Faker\Generator as Faker;
+
+$factory->define(Kargo::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
