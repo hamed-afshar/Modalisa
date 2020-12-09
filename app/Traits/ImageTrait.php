889fb0558391e2f259fd17aa4659f85a8de67c16
hamed-afshar <hamed.afshar.n@gmail.com>
2020-12-09 14:08:47 +0330

    Create ImageTrait and add both upload and delete image to it

--- app/Traits/ImageTrait.php
+++ app/Traits/ImageTrait.php
@@ -10,12 +10,17 @@ use Illuminate\Support\Str;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
-trait UploadTrait
+trait ImageTrait
 {
     public function uploadOne(UploadedFile $uploadFile, $folder = null, $disk = 'public', $filename = null)
     {
         $name = !is_null($filename) ? $filename : Str::random(25);
         $file = $uploadFile->storeAs($folder, $name. '.' .$uploadFile->getClientOriginalExtension(), $disk);
         return $file;
+    }
+
+    public function deleteOne($disk='public', $imageName = null)
+    {
+        Storage::disk($disk)->delete($imageName);
     }
 }
