4ad6978b505a59e6fb02457ea3d56121cdcd72b3
hamed-afshar <hamed.afshar.n@gmail.com>
2020-12-12 17:07:02 +0330

    Create CostManagementTest.php

---
+++ tests/Feature/CostManagementTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CostManagementTest extends TestCase
+{
+   use RefreshDatabase, WithFaker;
+    /** @test
+     * for Order model
+     */
+
+    /** @test
+     * for Product model
+     */
+    /** @test
+     * for Transaction model
+     */
+    /** @test
+     * for Kargo model
+     */
+}
