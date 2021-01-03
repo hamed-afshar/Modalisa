7044fea593599774f78fa4e7c32d8449c41f9a5d
hamed-afshar <hamed.afshar.n@gmail.com>
2021-01-03 11:31:52 +0330

    Update 2014_10_12_000000_create_users_table.php
    Decided to remove default valur for both subscription and role coulmns in this table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('subscription_id')->default(1)->nullable();
-            $table->unsignedBigInteger('role_id')->default(1)->nullable();
+            $table->unsignedBigInteger('subscription_id')->nullable();
+            $table->unsignedBigInteger('role_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
