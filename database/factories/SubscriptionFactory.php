dac9473d9500ddc47bbd8b09f71a9a8b5823b5de
hamed-afshar <hamed.afshar.n@gmail.com>
2021-01-13 15:52:37 +0330

    Update SubscriptionFactory.php

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -9,6 +9,6 @@ $factory->define(Subscription::class, function (Faker $faker) {
     return [
         'plan' => $faker->unique()->randomElement($array = array('Basic','White', 'Gold', 'Silver', 'Bronze')),
         'cost_percentage' => $faker->randomElement($array = array(0, 10, 20, 30)),
-        'kargo_limit' => $faker->randomElement($array = array(0, 10, 20, 30)),
+        'kargo_limit' => 5,
     ];
 });
