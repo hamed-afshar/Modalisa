714f8835b9fa2c0747f69e0e24ef767e5e337747
hamed-afshar <hamed.afshar.n@gmail.com>
2021-02-01 16:25:27 +0330

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleSeeder::class);
         $this->call(SubscriptionSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(CustomerSeeder::class);
     }
 }
