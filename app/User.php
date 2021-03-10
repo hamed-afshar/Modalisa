db883fa9509db8e7c79051295aa29b8199d99b25
hamed-afshar <hamed.afshar.n@gmail.com>
2021-03-11 01:14:42 +0330

    Update User.php

--- app/User.php
+++ app/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     public function path()
     {
-        return "/users/{$this->id}";
+        return "api/users/{$this->id}";
     }
 
 
