e7d37639625dba07f37024a4a12555c34df45312
hamed-afshar <hamed.afshar.n@gmail.com>
2021-03-10 17:06:20 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+Route::post('/register', 'API\AuthController@register');
+Route::post('/login', 'API\AuthController@login');
+
+Route::get('/users', 'API\UserController@index');
+//    Route::get('/users', 'API\UserController@index')->name('users.index');
+//    Route::get('/users/{user}', 'API\UserController@show')->name('users.show');
+//    Route::get('/users/{user}/edit', 'API\UserController@edit')->name('users.edit');
+//    Route::patch('/users/{user}', 'API\UserController@update')->name('users.update');
+//    Route::delete('/users/{user}', 'API\UserController@destroy')->name('users.destroy');
+//    Route::patch('/edit-profile/{user}', 'API\UserController@editProfile')->name('users.profile');
